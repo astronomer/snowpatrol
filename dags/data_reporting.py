c7f07aae6effcd763f1e5b1c1f4087484ab361b1
Olivier Daneau <olivier.daneau@astronomer.io>
2024-03-08 17:09:29 -0500

    add usage date to reporting_storage_cost

--- dags/data_reporting.py
+++ dags/data_reporting.py
@@ -119,10 +119,12 @@ with DAG(
         conn_id=snowflake_conn_id,
         outlets=reporting_storage_cost,
         sql=f"""
-       CREATE OR REPLACE TABLE {reporting_storage_cost.uri} AS
-        SELECT database_name,
+        CREATE OR REPLACE TABLE {reporting_storage_cost.uri} AS
+        SELECT 
+            usage_date,
             database_id,
-            AVERAGE_DATABASE_BYTES AS database_bytes,
+            database_name,
+            average_database_bytes AS database_bytes,
             (database_bytes / POWER(1024, 4)) * 23.00 AS database_storage_cost,
             SYSDATE() AS last_updated_at
         FROM snowflake.account_usage.database_storage_usage_history
