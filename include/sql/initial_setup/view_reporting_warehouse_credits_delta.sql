4b90334a4ae4553009b421b4da16a3a167ad13e4
Olivier Daneau <olivier.daneau@astronomer.io>
2024-06-11 15:23:12 -0400

    Lint and format files

--- include/sql/initial_setup/view_reporting_warehouse_credits_delta.sql
+++ include/sql/initial_setup/view_reporting_warehouse_credits_delta.sql
@@ -3,8 +3,7 @@ CREATE OR REPLACE VIEW {{ params.view_name }} AS (
         T.WAREHOUSE_NAME,
         T.WAREHOUSE_CREDITS,
         T.DS,
-        T.WAREHOUSE_CREDITS
-        - Y.WAREHOUSE_CREDITS AS WAREHOUSE_CREDITS_DELTA_YESTERDAY
+        T.WAREHOUSE_CREDITS - Y.WAREHOUSE_CREDITS AS WAREHOUSE_CREDITS_DELTA_YESTERDAY
     FROM {{ params.reporting_warehouse_credits }} AS T
     LEFT JOIN {{ params.reporting_warehouse_credits }} AS Y
         ON
